e7c1fdf111ea5ad33ffbf17649dbecc596c01d78
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-04-22 16:17:54 +0530

    topper section undone

--- client/client/src/pages/landingPage/components/Toppers.js
+++ client/client/src/pages/landingPage/components/Toppers.js
@@ -7,7 +7,7 @@ function Toppers() {
         <img src={trophy} alt="Trophy" />
       </div>
       <div className="Toppers__list">
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic recusandae eum inventore. Totam exercitationem a vero inventore aspernatur deleniti, ipsam assumenda perferendis itaque ut! Asperiores voluptatibus ipsum blanditiis sit enim.</p>
+         <span className="vertical__line"></span>
       </div>
     </div>
   );
