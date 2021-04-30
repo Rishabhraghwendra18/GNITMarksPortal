16075c7b0499fb1b58a18209c78c8513b3ce086e
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-04-30 17:22:31 +0530

    Added my photo in profile section

--- client/client/src/pages/landingPage/components/MyProfile.js
+++ client/client/src/pages/landingPage/components/MyProfile.js
@@ -1,14 +1,19 @@
 import React from "react";
+import myPic from "../../../assets/my-pic.jpg";
 
 function MyProfile() {
   return (
     <div className="Profile flex__container">
       <div className="Profile__mine flex__container">
         <div className="about__me flex__container">
-          <p>"A passionte developer,<br/> 
-          who loves to innovate new things"</p>
+          <p>
+            "A passionte developer,
+            <br /> <span></span>
+            who loves to create innovations"
+          </p>
         </div>
         <div className="mine__photo flex__container">
+          <img src={myPic} alt="My Pic" />
           <p>Rishabh Raghwendra</p>
           <q>CSE Student,GNIT</q>
         </div>
