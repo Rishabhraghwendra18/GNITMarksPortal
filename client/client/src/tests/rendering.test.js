defdc3c4ff33e8f4be4cc2f6ec570f2212df44f1
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-07-13 17:51:33 +0530

    formated rendering.test.js

--- client/client/src/tests/rendering.test.js
+++ client/client/src/tests/rendering.test.js
@@ -4,7 +4,7 @@ import Home from "../pages/Home";
 import Hero from "../pages/landingPage/components/Hero";
 import Login from "../pages/landingPage/components/Login";
 import MyProfile from "../pages/landingPage/components/MyProfile";
-import Toppers from  "../pages/landingPage/components/Toppers";
+import Toppers from "../pages/landingPage/components/Toppers";
 import NavBar from "../pages/landingPage/components/NavBar";
 
 describe("are Landing page components rendering", () => {
@@ -40,51 +40,50 @@ describe("are Landing page components rendering", () => {
   });
 });
 
-describe('is elements in each component rendering', () => {
-      it("is Hero rendering quote & img",()=>{
-            const {queryByTitle} = render(<Hero/>);
-            const heroQuote=queryByTitle("Hero_Quote");
-            const heroImage=queryByTitle("Hero_Image");
-            expect(heroQuote).toBeTruthy();
-            expect(heroImage).toBeTruthy();
-      });
-      it("is Login rendering login box & img",()=>{
-            const {queryByTitle} = render(<Login/>);
-            const loginBox=queryByTitle("Login_Box");
-            const LoginImage=queryByTitle("Login_Image");
-            expect(loginBox).toBeTruthy();
-            expect(LoginImage).toBeTruthy();
-      });
-      it("is MyProfile rendering all stuffs",()=>{
-            const {queryByTitle} = render(<MyProfile/>);
-            const heading=queryByTitle("heading");
-            const aboutMe=queryByTitle("about_me");
-            const myPic=queryByTitle("mine_pic");
-            const myName=queryByTitle("my_name");
-            const title=queryByTitle("title");
-            expect(heading).toBeTruthy();
-            expect(aboutMe).toBeTruthy();
-            expect(myPic).toBeTruthy();
-            expect(myName).toBeTruthy();
-            expect(title).toBeTruthy();
-      });
-      it("is Topper rendering toppers list & img",()=>{
-            const {queryByTitle} = render(<Toppers/>);
-            const toppersImage=queryByTitle("Toppers__image");
-            const toppersList=queryByTitle("toppers_list");
-            expect(toppersImage).toBeTruthy();
-            expect(toppersList).toBeTruthy();
-      });
-      it("is NavBar rendering links & logo",()=>{
-            const {queryByTitle} = render(<NavBar/>);
-            const navBarLogo=queryByTitle("navbar_logo");
-            const homeAnchorTag=queryByTitle("home_anchor_tag");
-            const loginAnchorTag=queryByTitle("login_anchor_tag");
-            const contactAnchorTag=queryByTitle("contact_anchor_tag");
-            expect(navBarLogo).toBeTruthy();
-            expect(homeAnchorTag).toBeTruthy();
-            expect(loginAnchorTag).toBeTruthy();
-            expect(contactAnchorTag).toBeTruthy();
-      });
+describe("is elements in each component rendering", () => {
+  it("is Hero rendering quote & img", () => {
+    const { queryByTitle } = render(<Hero />);
+    const heroQuote = queryByTitle("Hero_Quote");
+    const heroImage = queryByTitle("Hero_Image");
+    expect(heroQuote).toBeTruthy();
+    expect(heroImage).toBeTruthy();
+  });
+  it("is Login rendering login box & img", () => {
+    const { queryByTitle } = render(<Login />);
+    const loginBox = queryByTitle("Login_Box");
+    const LoginImage = queryByTitle("Login_Image");
+    expect(loginBox).toBeTruthy();
+    expect(LoginImage).toBeTruthy();
+  });
+  it("is MyProfile rendering all stuffs", () => {
+    const { queryByTitle } = render(<MyProfile />);
+    const heading = queryByTitle("heading");
+    const aboutMe = queryByTitle("about_me");
+    const myPic = queryByTitle("mine_pic");
+    const myName = queryByTitle("my_name");
+    const title = queryByTitle("title");
+    expect(heading).toBeTruthy();
+    expect(aboutMe).toBeTruthy();
+    expect(myPic).toBeTruthy();
+    expect(myName).toBeTruthy();
+    expect(title).toBeTruthy();
+  });
+  it("is Topper rendering toppers list & img", () => {
+    const { queryByTitle } = render(<Toppers />);
+    const toppersImage = queryByTitle("Toppers__image");
+    const toppersList = queryByTitle("toppers_list");
+    expect(toppersImage).toBeTruthy();
+    expect(toppersList).toBeTruthy();
+  });
+  it("is NavBar rendering links & logo", () => {
+    const { queryByTitle } = render(<NavBar />);
+    const navBarLogo = queryByTitle("navbar_logo");
+    const homeAnchorTag = queryByTitle("home_anchor_tag");
+    const loginAnchorTag = queryByTitle("login_anchor_tag");
+    const contactAnchorTag = queryByTitle("contact_anchor_tag");
+    expect(navBarLogo).toBeTruthy();
+    expect(homeAnchorTag).toBeTruthy();
+    expect(loginAnchorTag).toBeTruthy();
+    expect(contactAnchorTag).toBeTruthy();
+  });
 });
-
