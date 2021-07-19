729b33b47f0e6591c114aaa2c58d68b418730a56
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-07-19 17:12:19 +0530

    student marks displaying in table

--- client/client/src/pages/authPages/StudentDashboard.js
+++ client/client/src/pages/authPages/StudentDashboard.js
@@ -1,30 +1,54 @@
-import React,{useContext,useEffect} from "react";
-import {userLoginCredentialsContext}from "../App";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
+import { userLoginCredentialsContext } from "../App";
 
 function StudentDashboard() {
-  const userLoginCredentials=useContext(userLoginCredentialsContext);
+  const userLoginCredentials = useContext(userLoginCredentialsContext);
+  const userMarksInHTMLTag = [];
   useEffect(() => {
-    console.log("hi:",userLoginCredentials)
+    console.log("hi:", userLoginCredentials);
   }, []);
   return (
     <div className="StudentDashBoard flex__container">
       <div className="StudentDashBoard__marksDiv">
         <div className="studentDetailsAndSemesters">
           <div>
-            <p>Name: Rishabh</p>
-            <p>IPU ID: IP2020202</p>
+            <p>ID: {userLoginCredentials.body.id}</p>
           </div>
         </div>
         <div className="verticalLine"></div>
-        <div className="studentMarks">
-          <div className="subjects">
-            <h2>Subjects</h2>
-          </div>
-          <div className="verticalLine"></div>
-          <div className="marks">
-            <h2>Marks</h2>
-          </div>
-        </div>
+        <Table variant="simple" colorScheme="black" size="lg">
+          <TableCaption>nulls: marks has not been uploaded</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>subject</Th>
+              <Th>Marks</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {Object.entries(userLoginCredentials.body).map((e) => {
+              if (e[0] != "id") {
+                const subjects=e[0].split('_');
+                console.log("arrays: ",e)
+                return (
+                  <Tr>
+                    <Td>{subjects.map(e=>e.charAt(0).toUpperCase()+e.slice(1)+' ')}</Td>
+                    <Td>{e[1]?e[1]:"null"}</Td>
+                  </Tr>
+                );
+              }
+            })}
+          </Tbody>
+        </Table>
       </div>
     </div>
   );
