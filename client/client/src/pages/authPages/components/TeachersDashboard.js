29336354b33106fef8d2071621403717b4cc9649
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-07-19 19:12:41 +0530

    teacher dashboard showing students details

--- client/client/src/pages/authPages/components/TeachersDashboard.js
+++ client/client/src/pages/authPages/components/TeachersDashboard.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { userLoginCredentialsContext } from "../../App";
 
 function TeachersDashboard() {
-      return (
-            <div className="TeachersDashboard flex__container">
-                  <Link to={'/uploadmarks'}><button className="uploadButton">Upload Marks</button></Link>
-                  <div className="dashboard">
-                  <div className="teacherInformation"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, impedit.</p></div>
-                  <div className="teachersStudentMarks">
-                        <table>
-                              <tr>
-                                    <th>Student Name</th>
-                                    <th>Marks</th>
-                              </tr>
-                        </table>
-                  </div>
-                  </div>
-            </div>
-      )
+  const userLoginCredentials = useContext(userLoginCredentialsContext);
+  return (
+    <div className="TeachersDashboard flex__container">
+      <Link to={"/uploadmarks"}>
+        <button className="uploadButton">Upload Marks</button>
+      </Link>
+      <div className="dashboard">
+        <div className="teacherInformation">
+          <p>{userLoginCredentials.body.id}</p>
+        </div>
+        <div className="teachersStudentMarks">
+          <Table variant="simple" colorScheme="black" size="lg">
+            <Thead>
+              <Tr>
+                <Th>Student ID</Th>
+                <Th>Student Name</Th>
+                <Th>Student Branch</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {userLoginCredentials.body.map((e) => {
+                const studentList = [];
+                for (const property in e) {
+                  studentList.push(
+                        <Td>{e[property]}</Td>
+                  )
+                }
+                return(
+                      <Tr>
+                            {studentList.map(e=>e)}
+                      </Tr>
+                )
+              })}
+            </Tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TeachersDashboard
+export default TeachersDashboard;
