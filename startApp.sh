82ea2e70a13fb40f8b5a536809532cd77064cad1
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-08-18 16:53:43 +0530

    removed unused shell script

--- startApp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "======================================"
-echo starting FrontEnd
-cd client/client && npm start
-
-echo "====================================="
-echo starting BackEnd
