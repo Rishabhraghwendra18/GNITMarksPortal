d35225d3832280fedafb9a470544f1078bbf3f66
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-09-03 17:55:59 +0530

    properly wrote file path for build folder

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV == "production") {
   const path = require("path");
-  app.use(express.static(path.join(__dirname, "/build")));
+  app.use(express.static(path.join(__dirname, "./build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
   });
